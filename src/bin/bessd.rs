36d833acaaba3db2759a2b1ccadd18621c0a6ed9
Adewale Ayokanmi Adeleye <adewagold@gmail.com>
2022-12-23 09:09:45 +0100

    daemonize  src/bin/bessd.rs
    daemonize src/bin/bessd.rs

--- src/bin/bessd.rs
+++ src/bin/bessd.rs
@@ -1,5 +1,6 @@
 use bess::bessd;
 use bess::opts::*;
+use bess::daemon::*;
 // use bess::bessctl;
 use log::*;
 use clap::Parser;
@@ -28,7 +29,7 @@ async fn main() {
         info!("Launching BESS daemon in process mode...");
     } else {
         info!("Launching BESS daemon in background...");
-
+        start_daemon();
         // if flags.logtostderr == true || flags.alsologtostderr == true {
         //   flags.logtostderr = false;
         //   flags.alsologtostderr = false;
---
+++ src/core/daemon.rs
@@ -0,0 +1,29 @@
+
+use std::{fs::File};
+use daemonize::Daemonize;
+
+const STD_OUT_FILE_PATH:&str =  "/tmp/bessd.out";
+const STD_ERR_FILE_PATH:&str = "/tmp/bessd.err";
+const PID_FILE_PATH:&str = "/tmp/bessd.pid";
+
+pub fn start_daemon(){
+
+    let std_out = File::create(STD_OUT_FILE_PATH).unwrap();
+    let std_err = File::create(STD_ERR_FILE_PATH).unwrap();
+    let daemonize = Daemonize::new()
+        .pid_file(PID_FILE_PATH) // Every method except `new` and `start`
+        .chown_pid_file(true)      // is optional, see `Daemonize` documentation
+        .working_directory("/tmp") // for default behaviour.
+        .group("daemon") // Group name
+        .user("unknown")
+        .group(2)        // or group id.
+        .umask(0o777)    // Set umask, `0o027` by default.
+        .stdout(std_out)  // Redirect stdout to `/tmp/daemon.out`.
+        .stderr(std_err)  // Redirect stderr to `/tmp/daemon.err`.
+        .privileged_action(|| "Executed before drop privileges");
+
+    match daemonize.start() {
+        Ok(_) => eprintln!("Success, started daemon successfully"),
+        Err(e) => eprintln!("Error, {}", e),
+        }
+}
